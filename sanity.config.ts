f93a0807a06a73de64009eb7a529e98c105c00d6
Yuki Maeda <47184244+snowyuki31@users.noreply.github.com>
2024-09-26 02:55:50 +0900

    conf: add sanity-plugin-markdown

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
 import { schema } from "./src/sanity/schemaTypes";
+import { markdownSchema } from "sanity-plugin-markdown";
 
 export default defineConfig({
   projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID,
   dataset: import.meta.env.PUBLIC_SANITY_DATASET,
-  plugins: [structureTool()],
+  plugins: [structureTool(), markdownSchema()],
   schema,
 });
