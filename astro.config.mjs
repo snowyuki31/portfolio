477bd2f9c35d1e2c3887ca6a24e982fe3b3425f2
Yuki Maeda <47184244+snowyuki31@users.noreply.github.com>
2024-09-26 01:25:29 +0900

    init: add sanity integration config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,27 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { loadEnv } from "vite";
 
-import sanity from '@sanity/astro';
-import react from '@astrojs/react';
+import { defineConfig } from "astro/config";
+
+import sanity from "@sanity/astro";
+import react from "@astrojs/react";
+
+const { PUBLIC_SANITY_PROJECT_ID, PUBLIC_SANITY_DATASET } = loadEnv(
+  process.env.NODE_ENV ?? "",
+  process.cwd(),
+  ""
+);
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sanity(), react()]
+  integrations: [
+    sanity({
+      projectId: PUBLIC_SANITY_PROJECT_ID,
+      dataset: PUBLIC_SANITY_DATASET,
+      useCdn: false,
+      apiVersion: "2024-07-24",
+      studioBasePath: "/studio",
+    }),
+    react(),
+  ],
 });
