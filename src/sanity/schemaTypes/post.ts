98a355565fd5ad18c892f18fc889f0df8145c596
Yuki Maeda <47184244+snowyuki31@users.noreply.github.com>
2024-09-26 02:00:46 +0900

    feat: add post schema

---
+++ src/sanity/schemaTypes/post.ts
@@ -0,0 +1,50 @@
+import { defineField, defineType } from "sanity";
+
+export const postType = defineType({
+  name: "post",
+  type: "document",
+  fields: [
+    defineField({
+      name: "title",
+      type: "string",
+    }),
+    defineField({
+      name: "slug",
+      type: "slug",
+    }),
+    defineField({
+      name: "mainImage",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+      fields: [
+        {
+          name: "alt",
+          type: "string",
+          title: "Alternative Text",
+        },
+      ],
+    }),
+    defineField({
+      name: "publishedAt",
+      type: "datetime",
+    }),
+    // defineField({
+    //   name: "body",
+    //   type: "blockContent",
+    // }),
+  ],
+
+  preview: {
+    select: {
+      title: "title",
+      author: "author.name",
+      media: "mainImage",
+    },
+    prepare(selection) {
+      const { author } = selection;
+      return { ...selection, subtitle: author && `by ${author}` };
+    },
+  },
+});
