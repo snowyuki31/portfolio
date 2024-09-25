a541d838ff1d5d453140101170438c5c86c7d604
Yuki Maeda <47184244+snowyuki31@users.noreply.github.com>
2024-09-26 01:13:23 +0900

    init: setup sanity integration for astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sanity from '@sanity/astro';
+import react from '@astrojs/react';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [sanity(), react()]
+});
