05313e9da295673a5100cb700c633d92612f6edc
Yuki Maeda <47184244+snowyuki31@users.noreply.github.com>
2024-10-01 02:19:44 +0900

    feat: add type-gen

--- sanity.config.ts
+++ sanity.config.ts
@@ -4,8 +4,12 @@ import { schema } from "./src/sanity/schemaTypes";
 import { markdownSchema } from "sanity-plugin-markdown";
 
 export default defineConfig({
-  projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID,
-  dataset: import.meta.env.PUBLIC_SANITY_DATASET,
+  projectId: import.meta.env
+    ? import.meta.env.PUBLIC_SANITY_PROJECT_ID
+    : process.env.PUBLIC_SANITY_PROJECT_ID,
+  dataset: import.meta.env
+    ? import.meta.env.PUBLIC_SANITY_DATASET
+    : process.env.PUBLIC_SANITY_DATASET,
   plugins: [structureTool(), markdownSchema()],
   schema,
 });
